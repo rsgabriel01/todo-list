5de567f84b4e3b5d372c832c886ccc05c0123161
gabriel.souza <gabriel.souza@aliare.co>
2024-02-07 13:48:44 -0300

    alter name logo img

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,11 @@
 import styles from './Header.module.css'
 
-import igniteLogo from '/todo-logo-rocket.svg'
+import todoListLogo from '/todo-logo-rocket.svg'
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="Logotipo do ToDo  List" />
+      <img src={todoListLogo} alt="Logotipo do ToDo  List" />
       <span>to</span>
       <span>do</span>
     </header>
